f8865cb3b9dd260ee60b4d85e9d5924e64533b39
Aragorant <6947476@gmail.com>
2023-09-25 14:07:02 +0300

    Initial commit

---
+++ Task1.py
@@ -0,0 +1,79 @@
+import csv
+import random
+from faker import Faker
+from faker.providers import BaseProvider
+
+class CustomProvider(BaseProvider):
+    def female_middle_name(self):
+        female_middle_names = [
+            "Миколаївна",
+            "Володимирівна",
+            "Олександрівна",
+            "Іванівна",
+            "Василівна",
+            "Сергіївна",
+            "Вікторівна",
+            "Михайлівна"
+        ]
+        return self.random_element(female_middle_names)
+
+    def male_middle_name(self):
+        male_middle_names = [
+            "Миколайович",
+            "Володимирович",
+            "Олександрович",
+            "Іванович",
+            "Васильович",
+            "Сергійович",
+            "Вікторович",
+            "Михайлович"
+        ]
+        return self.random_element(male_middle_names)
+
+fake = Faker('uk_UA')
+fake.add_provider(CustomProvider)
+
+total_records = 2000
+
+records = []
+
+for i in range(total_records):
+    if i < total_records * 0.4:
+        gender = "Жіноча"
+        name = fake.first_name_female()
+        middle_name = fake.female_middle_name()
+
+    else:
+        gender = "Чоловіча"
+        name = fake.first_name_male()
+        middle_name = fake.male_middle_name()
+
+    birthdate = fake.date_of_birth(minimum_age=15, maximum_age=85)
+    record = {
+        "Прізвище": fake.last_name(),
+        "Ім’я": name,
+        "По батькові": middle_name,
+        "Стать": gender,
+        "Дата народження": birthdate.strftime('%d.%m.%Y'),
+        "Посада": fake.job(),
+        "Місто проживання": fake.city(),
+        "Адреса проживання": fake.address(),
+        "Телефон": fake.phone_number(),
+        "Email": fake.email()
+    }
+    records.append(record)
+
+random.shuffle(records)
+
+with open('employees.csv', mode='w', newline='', encoding='utf-8') as csv_file:
+    fieldnames = ["Прізвище", "Ім’я", "По батькові", "Стать", "Дата народження",
+                  "Посада", "Місто проживання", "Адреса проживання", "Телефон", "Email"]
+
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+    writer.writeheader()
+
+    for record in records:
+        writer.writerow(record)
+
+print("Генерація завершена. Файл employees.csv створено.")
---
+++ Task3.py
@@ -0,0 +1,103 @@
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+def calculate_age(birthdate):
+    today = datetime.today()
+    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+    return age
+
+def calculate_statistics():
+    try:
+        with open('employees.csv', mode='r', newline='', encoding='utf-8') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+
+            age_categories = {"younger_18": {"male": 0, "female": 0},
+                              "18-45": {"male": 0, "female": 0},
+                              "45-70": {"male": 0, "female": 0},
+                              "older_70": {"male": 0, "female": 0}}
+            gender_counts = {"male": 0, "female": 0}
+
+            for row in csv_reader:
+                birthdate = datetime.strptime(row["Дата народження"], '%d.%m.%Y')
+                age = calculate_age(birthdate)
+                if age < 18:
+                    age_category = "younger_18"
+                elif 18 <= age <= 45:
+                    age_category = "18-45"
+                elif 45 < age <= 70:
+                    age_category = "45-70"
+                else:
+                    age_category = "older_70"
+
+                if row["Стать"] == "Чоловіча":
+                    gender_category = "male"
+                    gender_counts["male"] += 1
+                elif row["Стать"] == "Жіноча":
+                    gender_category = "female"
+                    gender_counts["female"] += 1
+                else:
+                    continue
+
+                age_categories[age_category][gender_category] += 1
+
+            print("\nСтатистика за статтю:")
+            print(f"Чоловіки: {gender_counts['male']} співробітників")
+            print(f"Жінки: {gender_counts['female']} співробітників")
+
+            print("\nСтатистика за віком:")
+            for category, counts in age_categories.items():
+                print(f"{category}: {counts['male'] + counts['female']} співробітників")
+
+            print("\nСтатистика за статтю та віком:")
+            for category, counts in age_categories.items():
+                print(f"{category}: Чоловіки - {counts['male']}, Жінки - {counts['female']}")
+
+            build_age_bar_chart(age_categories)
+            build_gender_age_stacked_bar_chart(age_categories)
+            build_gender_pie_chart(gender_counts)
+
+            print("Ok")
+
+    except FileNotFoundError:
+        print("Повідомлення про відсутність, або проблеми при відкритті файлу CSV.")
+    except Exception as e:
+        print("Помилка:", str(e))
+
+def build_age_bar_chart(age_categories):
+    categories = list(age_categories.keys())
+    total_counts = [counts["male"] + counts["female"] for counts in age_categories.values()]
+    plt.figure()
+    plt.bar(categories, total_counts, color='teal')
+    plt.xlabel('Вікова категорія')
+    plt.ylabel('Кількість співробітників')
+    plt.title('Статистика за віком')
+    plt.xticks(rotation=45)
+
+def build_gender_age_stacked_bar_chart(age_categories):
+    categories = list(age_categories.keys())
+    male_counts = [counts["male"] for counts in age_categories.values()]
+    female_counts = [counts["female"] for counts in age_categories.values()]
+
+    bar_width = 0.35
+    index = range(len(categories))
+    plt.figure()
+    plt.bar(index, male_counts, bar_width, label='Чоловіки', color='blue')
+    plt.bar(index, female_counts, bar_width, label='Жінки', color='pink', bottom=male_counts)
+    plt.xlabel('Вікова категорія')
+    plt.ylabel('Кількість співробітників')
+    plt.title('Статистика за статтю та віком')
+    plt.xticks(index, categories, rotation=45)
+    plt.legend()
+
+def build_gender_pie_chart(gender_counts):
+    labels = 'Чоловіча стать', 'Жіноча стать'
+    sizes = [gender_counts["male"], gender_counts["female"]]
+    colors = ['blue', 'pink']
+    plt.figure()
+    plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
+    plt.axis('equal')
+    plt.title('Статистика за статтю')
+
+calculate_statistics()
+plt.show()
